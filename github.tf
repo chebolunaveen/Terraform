77c4ebd0b716efb21c31a24acdefdd2ac4d6eb31
Ubuntu <ubuntu@ip-172-31-88-61.ec2.internal>
2023-07-22 13:17:28 +0000

    first commit

---
+++ github.tf
@@ -0,0 +1,7 @@
+
+resource "github_repository" "example" {
+  name = "Terraform"
+  description = "Repo is created using  Terraform"
+  visibility = "public"
+
+}
---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "github"{
+   token = "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"
+
+}
